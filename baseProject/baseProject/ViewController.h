dcb583e07a6d0132a51dcf4df156b79d97710cdf
Scott Waite <scomttwaite@gmail.com>
2013-10-05 23:32:41 -0400

    Week 1

---
+++ baseProject/baseProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  baseProject
+//
+//  Created on 9/4/13.
+//  Copyright (c) 2013 FullSail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ baseProject/baseProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  baseProject
+//
+//  Created on 9/4/13.
+//  Copyright (c) 2013 FullSail. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
