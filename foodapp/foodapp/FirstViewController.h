df73202d5270ad3e076ed7f12ca3f88eea3f7955
Scott Waite <scomttwaite@gmail.com>
2013-10-05 23:35:48 -0400

    FoodApp

---
+++ foodapp/foodapp/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  foodapp
+//
+//  Created by Scott Waite on 10/5/13.
+//  Copyright (c) 2013 Scott Waite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ foodapp/foodapp/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  foodapp
+//
+//  Created by Scott Waite on 10/5/13.
+//  Copyright (c) 2013 Scott Waite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
