1f04f9bc02103002f96f3ec41a2012b6e4aff703
Scott Waite <scomttwaite@gmail.com>
2013-10-05 23:34:34 -0400

    FoodApp

---
+++ AOC Week1/foodapp/foodapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  foodapp
+//
+//  Created by Scott Waite on 10/5/13.
+//  Copyright (c) 2013 Scott Waite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC Week1/foodapp/foodapp/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  foodapp
+//
+//  Created by Scott Waite on 10/5/13.
+//  Copyright (c) 2013 Scott Waite. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
